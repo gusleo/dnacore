5b6e2ed8138f7d379bb9edf2954d128a7a7db72b
Gus Leo <gusleo.bali@gmail.com>
2016-11-26 09:23:37 +0800

    few re-design structure of dna.core.service

--- dna.core/src/dna.core.applicationservice/Properties/AssemblyInfo.cs
+++ dna.core/src/dna.core.applicationservice/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("dna.core.applicationservice")]
+[assembly: AssemblyProduct("dna.core.appservice")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
